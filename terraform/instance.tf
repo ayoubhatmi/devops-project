6f884675032334f9d58ca751adfdd1bb13dc9059
ayhatmi <ayoub.hatmi18@gmail.com>
2024-04-28 17:17:03 +0200

    Fix Trafeak & Nextcloud

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,35 +1,56 @@
-
-data "openstack_images_image_v2" "image"{
-    name="Official-debian-11"
-    tags=["latest"]
+data "openstack_images_image_v2" "image" {
+    name = "Official-debian-11"
+    tags = ["latest"]
 }
+
 resource "openstack_compute_instance_v2" "my-instance" {
-    name="instance-1"
-    image_id = data.openstack_images_image_v2.image.id
-    flavor_name= "v1.m1.d10"
+    name            = "instance-1"
+    image_id        = data.openstack_images_image_v2.image.id
+    flavor_name     = "v1.m1.d10"
     security_groups = ["default"]
-    user_data = file("./conf.yml")
+    user_data       = file("./conf.yml")
     network {
         name = openstack_networking_network_v2.network.name
     }
 }
 
 resource "openstack_networking_floatingip_v2" "fip_1" {
-  pool = "public"
+    pool = "public"
 }
 
 resource "openstack_compute_floatingip_associate_v2" "fip_1" {
-  floating_ip = openstack_networking_floatingip_v2.fip_1.address
-  instance_id = openstack_compute_instance_v2.my-instance.id
+    floating_ip = openstack_networking_floatingip_v2.fip_1.address
+    instance_id = openstack_compute_instance_v2.my-instance.id
 }
 
 resource "openstack_blockstorage_volume_v2" "database_volume" {
-  name = "database-volume"
-  size = 1 
+    name = "database-volume"
+    size = 1 
 }
 
 resource "openstack_compute_volume_attach_v2" "attach_volume" {
-  instance_id = openstack_compute_instance_v2.my-instance.id
-  volume_id   = openstack_blockstorage_volume_v2.database_volume.id
-  device      = "/dev/vdb" 
+    instance_id = openstack_compute_instance_v2.my-instance.id
+    volume_id   = openstack_blockstorage_volume_v2.database_volume.id
+    device      = "/dev/vdb" 
+}
+
+ resource "ovh_domain_zone_record" "projet" {
+  zone = "uca-devops.ovh"
+  subdomain = "projet.ayoub"
+  target = "185.34.141.134"
+  fieldtype = "A"
+}
+
+resource "ovh_domain_zone_record" "blog" {
+  zone = "uca-devops.ovh"
+  subdomain = "blog.ayoub"
+  target = "185.34.141.134"
+  fieldtype = "A"
+}
+
+resource "ovh_domain_zone_record" "cloud" {
+  zone = "uca-devops.ovh"
+  subdomain = "cloud.ayoub"
+  target = "185.34.141.134"
+  fieldtype = "A"
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,10 @@
 terraform {
   required_providers {
     openstack = {
-      source = "terraform-providers/openstack"
+      source = "terraform-provider-openstack/openstack"    
+      }
+      ovh = {
+        source = "ovh/ovh"
     }
   }
   required_version = ">= 1.0.0"
@@ -12,3 +15,11 @@ terraform {
 provider "openstack" {
   cloud = "tp1_devops_app"
 }
+
+provider "ovh" {
+  endpoint = "ovh-eu"
+  application_key    = "1598a9a15fae7f32"
+  application_secret = "fb1f1c3e068d5cbe2ddd5b71f7fcb84a"
+  consumer_key       = "7aad6987a4b6f72191ba25b93a367dbe"
+}
+
